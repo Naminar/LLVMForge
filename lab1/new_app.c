c6cb26ddee9b827d97f05165c28a967f32cd8194
Naminar <buravko.va@phystech.edu>
2024-10-11 19:16:34 +0300

    more complex way for app

--- lab1/new_app.c
+++ lab1/new_app.c
@@ -4,28 +4,30 @@
 
 void app() {
 
-  int f_cross_x, f_cross_y, s_cross_x, s_cross_y;
-  int stars_x = simRand() % SIM_X_SIZE;
-  int stars_y= simRand() % SIM_Y_SIZE;
+  int stars_x[N_STARS] = {};
+  int stars_y[N_STARS] = {};
 
+  for (int i = 0; i < N_STARS; ++i){
+    stars_x[i] = simRand() % SIM_X_SIZE;
+    stars_y[i] = simRand() % SIM_Y_SIZE;
+  }
 
-    for (int x_l = stars_x; x_l > 0; --x_l) {
-      simPutPixel(x_l, stars_y, 0xFFFF0000);
-      simFlush();
+  for (int star = 0; star < N_STARS; ++star){
+    for (int x_l = stars_x[star]; x_l > 0; --x_l) {
+      simPutPixel(x_l, stars_y[star], 0xFFFF0000 + star*25);
     }
 
-    for (int x_r = stars_x; x_r < SIM_X_SIZE; ++x_r) {
-      simPutPixel(x_r, stars_y, 0xFFFF0000);
-      simFlush();
+    for (int x_r = stars_x[star]; x_r < SIM_X_SIZE; ++x_r) {
+      simPutPixel(x_r, stars_y[star], 0xFFFF0000 + star*25);
     }
 
-    for (int y_up = stars_y; y_up < SIM_Y_SIZE; ++y_up) {
-      simPutPixel(stars_x, y_up, 0xFFFF0000);
-      simFlush();
+    for (int y_up = stars_y[star]; y_up < SIM_Y_SIZE; ++y_up) {
+      simPutPixel(stars_x[star], y_up, 0xFFFF0000 + star*25);
     }
 
-    for (int y_down = stars_y; y_down > 0; --y_down) {
-      simPutPixel(stars_x, y_down, 0xFFFF0000);
-      simFlush();
+    for (int y_down = stars_y[star]; y_down > 0; --y_down) {
+      simPutPixel(stars_x[star], y_down, 0xFFFF0000 + star*25);
     }
+    simFlush();
+  }
 }
