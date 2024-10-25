db70660b6566e216224585840f6b82b837d0d211
Naminar <buravko.va@phystech.edu>
2024-10-26 00:46:04 +0300

    new statistic generation

--- lab2/gen_statistic.py
+++ lab2/gen_statistic.py
@@ -6,15 +6,21 @@ parser.add_argument('-s', '--static', action='store_true')
 
 def addlabels(x,y):
     for i in range(len(x)):
-        plt.text(i, y[i]//2, y[i], ha = 'center', color='white', size=6)
+        plt.text(i, y[i]//2, y[i], ha = 'center', color='white', size=6, rotation = 90)
 
 def create_stat(stat, name='stat'):
     plt.figure(figsize = (18/2,9/2), dpi = 150)
     plt.bar(stat.keys(), stat.values(), width=0.6)
     plt.xticks(fontsize=6, rotation=90)
-    # addlabels(stat.keys(), list(stat.values()))
+    addlabels(stat.keys(), list(stat.values()))
     plt.tight_layout()
     plt.savefig(f'{name}.jpeg')
+
+def update_stat(stat:dict, line:str):
+    if line in stat.keys():
+        stat[line] += 1
+    else:
+        stat[line] = 1
 
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -36,12 +42,18 @@ if __name__ == '__main__':
     else:
         stat = dict()
         dump = open('stat_dyn.txt').read().split()
-        print(dump)
+        if not dump:
+            print('stat_dyn.txt is empty')
+
         for (i, word) in enumerate(dump):
             if i > 0:
                 line = f'{dump[i]}<<{dump[i-1]}'
-                if line in stat.keys():
-                    stat[line] += 1
-                else:
-                    stat[line] = 1
+                update_stat(stat, line)
+            if i > 1:
+                line = f'{dump[i]}<<{dump[i-1]}<<{dump[i-2]}'
+                update_stat(stat, line)
+                # if line in stat.keys():
+                #     stat[line] += 1
+                # else:
+                #     stat[line] = 1
         create_stat(stat, 'stat_dyn')
